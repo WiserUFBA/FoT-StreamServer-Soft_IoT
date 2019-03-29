bb1edb089a270c88e972fa7c3c8c496766c215da
Brenno <brennodemello@hotmail.com>
2019-03-29 02:08:50 -0300

    Repository initialization

---
+++ src/main/java/br/ufba/dcc/wiser/soft_iot/analytics/fot/streamserver/soft_iot/Activator.java
@@ -0,0 +1,16 @@
+package br.ufba.dcc.wiser.soft_iot.analytics.fot.streamserver.soft_iot;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+    public void start(BundleContext context) throws Exception {
+        // TODO add activation code here
+    }
+
+    public void stop(BundleContext context) throws Exception {
+        // TODO add deactivation code here
+    }
+
+}
